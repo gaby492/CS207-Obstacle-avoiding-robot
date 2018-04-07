a3446cd177229ede4adfc387711d4e6673cbb812
gaby492 <gabriela.dominguez@uabc.edu.mx>
2018-04-07 15:34:39 -0700

    Added the file Obstacle_Avoidance_Car. This original code was provided by Elegoo, the distribuitor of Elegoo's kit.

--- Obstacle_avoiding_robot_v2_ino/Obstacle_avoiding_robot_v2_ino.ino
+++ Obstacle_avoiding_robot_v2_ino/Obstacle_avoiding_robot_v2_ino.ino
@@ -1,27 +1,38 @@
 #include <NewPing.h>        //Ultrasonic sensor function library. You must install this library
 #include <Servo.h>          //Servo motor library. This is standard library
      
+Servo servo_motor; //our servo name
 
 //our L298N control pins
-const int LeftMotorForward = 7;
-const int LeftMotorBackward = 6;
-const int RightMotorForward = 4;
-const int RightMotorBackward = 5;
+const int  ENA = 5;
+const int ENB = 6;
+const int LeftMotorForward = 8;
+const int LeftMotorBackward = 7;
+const int RightMotorForward = 11;
+const int RightMotorBackward = 9;
 
 //sensor pins
-#define trig_pin A1 //analog input 1
-#define echo_pin A2 //analog input 2
+#define trig_pin A5 //analog input 1
+#define echo_pin A4 //analog input 2
+#define carSpeed 150
 
-#define maximum_distance 200
+#define maximum_distance 400 ///200
 boolean goesForward = false;
 int distance = 100;
+char getstr; //Defines a function that receives the Bluetooth character
 
 NewPing sonar(trig_pin, echo_pin, maximum_distance); //sensor function
-Servo servo_motor; //our servo name
+
 
 
 void setup(){
-
+  Serial.begin(9600);
+  
+  pinMode(echo_pin, INPUT);    //
+  pinMode(trig_pin, OUTPUT);  //
+  pinMode(ENA, OUTPUT);   //
+  pinMode(ENB, OUTPUT);   //
+  
   pinMode(RightMotorForward, OUTPUT);
   pinMode(LeftMotorForward, OUTPUT);
   pinMode(LeftMotorBackward, OUTPUT);
@@ -30,7 +41,7 @@ void setup(){
   servo_motor.attach(10); //our servo pin
 
   servo_motor.write(115);
-  delay(2000);
+  delay(100);  //2000
   distance = readPing();
   delay(100);
   distance = readPing();
@@ -42,35 +53,40 @@ void setup(){
 }
 
 void loop(){
-
+  getstr = Serial.read();
   int distanceRight = 0;
   int distanceLeft = 0;
-  delay(50);
+  delay(5);
 
-  if (distance <= 20){
-    moveStop();
-    delay(300);
-    moveBackward();
-    delay(400);
-    moveStop();
-    delay(300);
-    distanceRight = lookRight();
-    delay(300);
-    distanceLeft = lookLeft();
-    delay(300);
-
-    if (distance >= distanceLeft){
-      turnRight();
-      moveStop();
+  Serial.println("a");
+  ////Si recibe un "Forward"
+  if(getstr == 'a'){
+        
+        if (distance <= 20){
+          moveStop();
+          delay(300);
+          moveBackward();
+          delay(400);
+          moveStop();
+          delay(300);
+          distanceRight = lookRight();
+          delay(300);
+          distanceLeft = lookLeft();
+          delay(300);
+      
+          if (distanceRight >= distanceLeft){  //if (distance >= distanceLeft){
+            turnRight();
+            moveStop();
+          }
+          else{
+            turnLeft();
+            moveStop();
+          }
+        }
+        else{
+          moveForward(); 
+        }
     }
-    else{
-      turnLeft();
-      moveStop();
-    }
-  }
-  else{
-    moveForward(); 
-  }
     distance = readPing();
 }
 
@@ -103,11 +119,13 @@ int readPing(){
 }
 
 void moveStop(){
-  
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
   digitalWrite(RightMotorForward, LOW);
   digitalWrite(LeftMotorForward, LOW);
   digitalWrite(RightMotorBackward, LOW);
   digitalWrite(LeftMotorBackward, LOW);
+  Serial.println("Stop!");
 }
 
 void moveForward(){
@@ -116,28 +134,40 @@ void moveForward(){
 
     goesForward=true;
     
+    analogWrite(ENA, carSpeed);
+    analogWrite(ENB, carSpeed);
+    
     digitalWrite(LeftMotorForward, HIGH);
     digitalWrite(RightMotorForward, HIGH);
   
     digitalWrite(LeftMotorBackward, LOW);
     digitalWrite(RightMotorBackward, LOW); 
+    Serial.println("Forward");
   }
 }
 
 void moveBackward(){
 
   goesForward=false;
-
+  
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  
   digitalWrite(LeftMotorBackward, HIGH);
   digitalWrite(RightMotorBackward, HIGH);
   
   digitalWrite(LeftMotorForward, LOW);
   digitalWrite(RightMotorForward, LOW);
+  
+  Serial.println("Back");
   
 }
 
 void turnRight(){
 
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  
   digitalWrite(LeftMotorForward, HIGH);
   digitalWrite(RightMotorBackward, HIGH);
   
@@ -146,18 +176,22 @@ void turnRight(){
   
   delay(500);
   
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  
   digitalWrite(LeftMotorForward, HIGH);
   digitalWrite(RightMotorForward, HIGH);
   
   digitalWrite(LeftMotorBackward, LOW);
   digitalWrite(RightMotorBackward, LOW);
- 
   
-  
+  Serial.println("Right"); 
 }
 
 void turnLeft(){
-
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  
   digitalWrite(LeftMotorBackward, HIGH);
   digitalWrite(RightMotorForward, HIGH);
   
@@ -165,6 +199,9 @@ void turnLeft(){
   digitalWrite(RightMotorBackward, LOW);
 
   delay(500);
+
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
   
   digitalWrite(LeftMotorForward, HIGH);
   digitalWrite(RightMotorForward, HIGH);
