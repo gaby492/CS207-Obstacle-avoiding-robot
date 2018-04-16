e0a0c831f3f0d9771bcadcd698f5013a32d9afe3
Gabriela Domínguez <gabriela.dominguez@uabc.edu.mx>
2018-04-16 17:00:49 -0400

    Update Obstacle_Avoidance_Car.ino

--- Obstacle_Avoidance_Car/Obstacle_Avoidance_Car.ino
+++ Obstacle_Avoidance_Car/Obstacle_Avoidance_Car.ino
@@ -1,4 +1,17 @@
-//www.elegoo.com
+/* 
+Obstacle Avoiding Robot
+Gabriela Dominguez
+Date: April 16, 2018
+
+This code allows the Smart Robot Car V 3.0 by Elegoo to move forward, backward and stopping when the user enters the assigned chars
+for those movements which are 'f', 's' and 'b'. Those chars can be received as input via Bluetooth using the application provided by 
+ELEGOO or any other Bluetooth BLE terminal. Plus, the robot can detect obstacles at a distance of 25cm and avoid them by changing 
+its path.
+
+The original code was included in the Elegoo's CD Tutorial. Some modifications were applied to it.
+
+www.elegoo.com
+*/
 
 
 #include <Servo.h>  //servo library
@@ -6,21 +19,31 @@ Servo myservo;      // create servo object to control servo
 char getstr;
 
 
+//For sending and receiving data from the ultrasonic sensor
 int Echo = A4;  
 int Trig = A5; 
 
 #define ENA 5
 #define ENB 6
+
+//For controlling the left motor
 #define IN1 7
 #define IN2 8
+
+//For controlling the right motor
 #define IN3 9
 #define IN4 11
+
 #define carSpeed 150
 int rightDistance = 0, leftDistance = 0, middleDistance = 0;
 
+/*
+The functions back, forward, right and left control the speed of the motors and indicate which motor should be "LOW" or "HIGH"
+*/
+
 void forward(){ 
-  analogWrite(ENA, carSpeed); //80
-  analogWrite(ENB, carSpeed); //80
+  analogWrite(ENA, carSpeed); 
+  analogWrite(ENB, carSpeed); 
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
@@ -76,24 +99,9 @@ int Distance_test() {
   return (int)Fdistance;
 }  
 
-void verifyFloor() {
-  if(LT_M || LT_R || LT_L){
-    Serial.print(!digitalRead(4));
-    Serial.print(!digitalRead(10));
-    Serial.print(!digitalRead(2));
-    //forward();
-    back();
-    
-    //Let's check which path should take next
-    while(LT_R){left(); delay(360); };
-    while(LT_L){right(); delay(360);};  
-  }//else{stop();}
-  
-  Serial.println(" ");
-}
-
-
 void setup() { 
+  //Initializing all the pins and the serial communication
+  
   myservo.attach(3);  // attach servo on pin 3 to servo object
   Serial.begin(9600);     
   pinMode(Echo, INPUT);    
@@ -108,46 +116,58 @@ void setup() {
 }
 
 void loop() { 
-    getstr = Serial.read(); 
+    getstr = Serial.read();    //To read the chars from the Bluetooth terminal
     
-    myservo.write(90);  //setservo position according to scaled value
+    myservo.write(90);  //set servo position according to scaled value
     delay(500); 
-    middleDistance = Distance_test();
+    middleDistance = Distance_test();  // Before moving it needs to obtain the distance to the object in the front
 
-    if(middleDistance <= 20) { 
+    if(middleDistance <= 20) {    //If the distance between the robot and the object in the front is less or equals to 20cm...
       Serial.println("Obstacle");    
       stop();
+      
+      //Turns to the right and obtains the distance 
       delay(500);                         
       myservo.write(10);          
       delay(1000);      
       rightDistance = Distance_test();
       
+      //Goes back to its original position
       delay(500);
-      myservo.write(90);              
+      myservo.write(90); 
+      
+      //Turns to the left and obtains the distance 
       delay(1000);                                                  
       myservo.write(180);              
       delay(1000); 
       leftDistance = Distance_test();
       
+      //Goes back to its original position
       delay(500);
       myservo.write(90);              
       delay(1000);
+      
+      //If the distance to the right is bigger than the distance to the left, the robot will turn to the right
       if(rightDistance > leftDistance) {
         right();
         delay(360);
       }
+      //If the distance to the left is bigger than the distance to the right, the robot will turn to the left
       else if(rightDistance < leftDistance) {
         left();
         delay(360);
       }
+      //If the distance between the robot and an object is less than 20 cm to the left or to the right the car will go back.
       else if((rightDistance <= 20) || (leftDistance <= 20)) {
         back();
         delay(180);
       }
+      //In any other case, if the input was a 'f', the car will go forward
       else if(getstr=='f'){ 
           forward();
       }
     }  
+    //If there is no obstacle in the front and any of these characters were received, the car will go forward, backward or it will just stop.
     else if(getstr=='f'){
           forward();
     }else if(getstr=='b'){
